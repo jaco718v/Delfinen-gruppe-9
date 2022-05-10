3babdb98dad4bb2b812047ffe957ea86507bccdc
jaco718v <jaco718v@stud.kea.dk>
2022-05-10 09:58:23 +0200

    Merge

--- src/membership/RecordTime.java
+++ src/membership/RecordTime.java
@@ -3,4 +3,7 @@ package membership;
 public abstract class RecordTime {
   private SwimDiscipline swimDiscipline;
   private double recordInSeconds;
+  private int dayOfMonth;
+  private int month;
+  private int year;
 }
