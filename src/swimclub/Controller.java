340e5faab1908822da58c6f995540b03970f966f
mads94i5 <mads94i5@stud.kea.dk>
2022-05-11 10:42:30 +0200

    addUser()

--- src/swimclub/Controller.java
+++ src/swimclub/Controller.java
@@ -1,6 +1,6 @@
 package swimclub;
 
-import membership.Member;
+import membership.Enum;
 import membership.*;
 import ui.UI;
 
@@ -43,38 +43,109 @@ private User loggedInUser;
         return true;
     }
 
+    private void addUser() {
+        if (users.size() > 0) {
+            if ((loggedInUser.getUserType() == Enum.UserType.ADMIN) || (loggedInUser.getUserType() == Enum.UserType.CHAIRMAN)) {
+                String userName = "";
+                String userPassword = "";
+                String userType;
+
+                boolean enteredUserName = false;
+                while (!enteredUserName) {
+                    ui.displayPleaseTypeLoginName();
+                    userName = sc.nextLine();
+                    if ((!userName.equals("")) && (!userName.equals(" "))) {
+                        enteredUserName = true;
+                    }
+                    if (!enteredUserName) {
+                        ui.displayPleaseEnterValidUser(userName);
+                    }
+                }
+
+                boolean enteredUserPassword = false;
+                while (!enteredUserPassword) {
+                    ui.displayPleaseTypeLoginPassword();
+                    userPassword = sc.nextLine();
+                    if ((!userPassword.equals("")) && (!userPassword.equals(" "))) {
+                            enteredUserPassword = true;
+                        }
+                    if (!enteredUserPassword) {
+                        ui.displayBadPassword(userPassword);
+                    }
+                }
+
+                boolean enteredUserType = false;
+                while (!enteredUserType) {
+                    ui.displayPleaseSelectUserType();
+                    userType = sc.nextLine();
+                    switch (userType) {
+                        case "1" -> {
+                            users.add(new User(userName, userPassword, Enum.UserType.ADMIN));
+                            enteredUserType = true;
+                        }
+                        case "2" -> {
+                            users.add(new User(userName, userPassword, Enum.UserType.CHAIRMAN));
+                            enteredUserType = true;
+                        }
+                        case "3" -> {
+                            users.add(new User(userName, userPassword, Enum.UserType.CASHIER));
+                            enteredUserType = true;
+                        }
+                        case "4" -> {
+                            users.add(new User(userName, userPassword, Enum.UserType.COACH));
+                            enteredUserType = true;
+                        }
+                    }
+                    ui.addUser(enteredUserType);
+                    if (!enteredUserType) {
+                        ui.displayBadPassword(userType);
+                    }
+                }
+            } else {
+                ui.loggedInUserNoPrivilege();
+            }
+        } else {
+            ui.noRegisteredUsersCreatingAdmin();
+            users.add(new User("admin", "admin", Enum.UserType.ADMIN));
+        }
+    }
+
+    private void removeUser() {
+
+    }
+
     private void loginUser() {
         if (users.size() > 0) {
             String userName;
             int userIndex = -1;
             String userPassword;
 
-            boolean enteredMemberName = false;
-            while (!enteredMemberName) {
+            boolean enteredUserName = false;
+            while (!enteredUserName) {
                 ui.displayPleaseTypeLoginName();
                 userName = sc.nextLine();
                 for (int i = 0; i < users.size(); i++) {
                     if (users.get(i).getName().equals(userName)) {
                         userIndex = i;
-                        enteredMemberName = true;
+                        enteredUserName = true;
                     }
                 }
-                if (!enteredMemberName) {
-                    ui.displayPleaseEnterValidUser();
+                if (!enteredUserName) {
+                    ui.displayPleaseEnterValidUser(userName);
                 }
             }
 
-            boolean enteredMemberPassword = false;
-            while (!enteredMemberPassword) {
+            boolean enteredUserPassword = false;
+            while (!enteredUserPassword) {
                 ui.displayPleaseTypeLoginPassword();
                 userPassword = sc.nextLine();
                 if (users.get(userIndex) != null) {
                     if (users.get(userIndex).getPassword().equals(userPassword)) {
-                        enteredMemberPassword = true;
+                        enteredUserPassword = true;
                     }
                 }
-                ui.loginUser(enteredMemberPassword);
-                if (!enteredMemberPassword) {
+                ui.loginUser(enteredUserPassword);
+                if (!enteredUserPassword) {
                     ui.displayWrongPassword();
                 }
             }
