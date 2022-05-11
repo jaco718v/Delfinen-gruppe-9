c5e5245821af456c7686d6d1734cfef15f0f61a5
jaco718v <jaco718v@stud.kea.dk>
2022-05-11 09:53:15 +0200

    Added showExpectedSubscriptionFees

--- src/membership/Team.java
+++ src/membership/Team.java
@@ -8,6 +8,35 @@ public class Team {
   private Enum.TeamType teamType;
   private Enum.AgeGroup ageGroup;
 
+  public Team(Enum.TeamType teamType, Enum.AgeGroup ageGroup) {
+    this.teamType = teamType;
+    this.ageGroup = ageGroup;
+  }
+
+  public Enum.AgeGroup getAgeGroup() {
+    return ageGroup;
+  }
+
+  public int getActiveMembers(){
+    int activeMembers = 0;
+    for(Member member : memberList){
+      if (member.getActive()){
+        activeMembers++;
+      }
+    }
+    return activeMembers;
+  }
+
+  public int getMembersAboveAge(int age){
+    int membersAboveAge = 0;
+    for(Member member : memberList){
+      if (member.getAge()>age){
+        membersAboveAge++;
+      }
+    }
+    return membersAboveAge;
+  }
+
   public void setMemberList(ArrayList<Member> memberList) {
     this.memberList = memberList;
   }
--- src/swimclub/Controller.java
+++ src/swimclub/Controller.java
@@ -1,8 +1,12 @@
 package swimclub;
 
 import membership.Enum;
+import membership.Team;
+
+import java.util.ArrayList;
 
 public class Controller {
+  ArrayList<Team> teamArray = new ArrayList<>();
 
     public static void main(String[] args) {
 	   Controller con = new Controller();
@@ -12,5 +16,30 @@ public class Controller {
     private void run() {
 
     }
-    
+
+    private double showExpectedSubscriptionFees() {
+      double subscriptionJunior = 1000;
+      double subscriptionSenior = 1600;
+      double subscriptionPassive = 500;
+      int seniorThreshold = 60;
+      double seniorDiscount = 0.25;
+      double subscriptionSum = 0;
+      for (Team team : teamArray) {
+        subscriptionSum += team.getActiveMembers() * subscriptionJunior +
+            (team.getMemberList().size() - team.getActiveMembers()) * subscriptionPassive;
+        if(team.getAgeGroup()== Enum.AgeGroup.O18){
+        subscriptionSum-= subscriptionSenior*seniorDiscount*team.getMembersAboveAge(seniorThreshold);
+        }
+      }
+      return subscriptionSum;
+      }
+
+
+
+    private void createTeams(){
+      teamArray.add(new Team(Enum.TeamType.REGULAR,Enum.AgeGroup.U18));
+      teamArray.add(new Team(Enum.TeamType.REGULAR,Enum.AgeGroup.O18));
+      teamArray.add(new Team(Enum.TeamType.COMPETITIVE,Enum.AgeGroup.U18));
+      teamArray.add(new Team(Enum.TeamType.COMPETITIVE,Enum.AgeGroup.O18));
+    }
 }
