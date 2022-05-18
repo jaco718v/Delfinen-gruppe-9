9efae7bc6d336f889d5a1d326017540522323677
mads94i5 <mads94i5@stud.kea.dk>
2022-05-18 11:40:24 +0200

    Fixed name capitalization editMember

--- src/swimclub/Controller.java
+++ src/swimclub/Controller.java
@@ -220,6 +220,39 @@ public class Controller {
         }
     }
 
+    private String addMemberId() {
+        ArrayList<String[]> memberData = fileHandler.readCSV("Members.csv");
+        String returnValue = "FULL";
+        int memberIdInt = 0;
+        int lastIndex = memberData.size() - 1;
+        if (lastIndex != -1) {
+            String[] memberArray = memberData.get(lastIndex);
+            if (tryParseInt(memberArray[0])) {
+                memberIdInt = Integer.parseInt(memberArray[0]);
+            }
+        }
+        if (memberIdInt + 1 < 10000) {
+            memberIdInt += 1;
+            returnValue = String.format("%04d", memberIdInt);
+        } else {
+            boolean foundNewMemberId = false;
+            for (int i = 1; i < 10000; i++) {
+                for (String[] memberArray : memberData) {
+                    int thisMemberIdInt = Integer.parseInt(memberArray[0]);
+                    if (thisMemberIdInt != i) {
+                        foundNewMemberId = true;
+                        break;
+                    }
+                }
+                if (foundNewMemberId) {
+                    returnValue = String.format("%04d", i);
+                    break;
+                }
+            }
+        }
+        return returnValue;
+    }
+
     private String addMemberName(){
         String memberName  = "";
         boolean enteredMemberName = false;
@@ -300,39 +333,6 @@ public class Controller {
         }
     }
 
-    private String addMemberId() {
-        ArrayList<String[]> memberData = fileHandler.readCSV("Members.csv");
-        String returnValue = "FULL";
-        int memberIdInt = 0;
-        int lastIndex = memberData.size() - 1;
-        if (lastIndex != -1) {
-            String[] memberArray = memberData.get(lastIndex);
-            if (tryParseInt(memberArray[0])) {
-                memberIdInt = Integer.parseInt(memberArray[0]);
-            }
-        }
-        if (memberIdInt + 1 < 10000) {
-            memberIdInt += 1;
-            returnValue = String.format("%04d", memberIdInt);
-        } else {
-            boolean foundNewMemberId = false;
-            for (int i = 1; i < 10000; i++) {
-                for (String[] memberArray : memberData) {
-                    int thisMemberIdInt = Integer.parseInt(memberArray[0]);
-                    if (thisMemberIdInt != i) {
-                        foundNewMemberId = true;
-                        break;
-                    }
-                }
-                if (foundNewMemberId) {
-                    returnValue = String.format("%04d", i);
-                    break;
-                }
-            }
-        }
-        return returnValue;
-    }
-
 
    public void editMember() {
         if ((loggedInUser.getUserType() == Enum.UserType.ADMIN) || (loggedInUser.getUserType() == Enum.UserType.CHAIRMAN)) {
@@ -362,19 +362,6 @@ public class Controller {
             ui.loggedInUserNoPrivilege();
         }
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
 
     public void removeMember() {
         if ((loggedInUser.getUserType() == Enum.UserType.ADMIN) || (loggedInUser.getUserType() == Enum.UserType.CHAIRMAN)) {
